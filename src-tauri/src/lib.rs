f29fb5507f74835a427bfbd1af9a093fab457825
LionelPastranaMK500 <andriypastrana1@gmail.com>
2026-03-03 22:16:58 -0500

    feat: update dependencies and improve plugin setup for Tauri application

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,13 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::new().build())
-        .setup(|app| {
-            if cfg!(debug_assertions) {
-                app.handle().plugin(
-                    tauri_plugin_log::Builder::default()
-                        .level(log::LevelFilter::Info)
-                        .build(),
-                )?;
-            }
+        .plugin(tauri_plugin_sql::Builder::default().build())
+        .plugin(
+            tauri_plugin_log::Builder::default()
+                .level(log::LevelFilter::Info)
+                .build(),
+        )
+        .setup(|_app| {
             Ok(())
         })
         .run(tauri::generate_context!())
