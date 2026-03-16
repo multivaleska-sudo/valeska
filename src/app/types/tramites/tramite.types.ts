bea59229eb5a4806b7e6702dd8d5695791e1958f
LavenderEdit <gercermagden@gmail.com>
2026-03-16 10:56:42 -0500

    feat: added a new form data for the tramite type

--- src/app/types/tramites/tramite.types.ts
+++ src/app/types/tramites/tramite.types.ts
@@ -26,6 +26,55 @@ export interface TramiteData {
   empresa: string;
 }
 
+export interface TramiteFormData {
+  // DEL TRAMITE
+  id?: number;
+  tramite_anio: string;
+  cliente: string;
+  telefono: string;
+  dni: string;
+  n_titulo: string;
+  tipo_tramite: string;
+  estado_tramite: string;
+  observaciones: string;
+  fecha_presentacion: string;
+  check_entrega_tarjeta: boolean;
+  fecha_entrega_tarjeta: string;
+  check_entrega_placa: boolean;
+  fecha_entrega_placa: string;
+  codigo_verificacion: string;
+
+  // DEL VEHICULO
+  vehiculo_marca: string;
+  vehiculo_motor: string;
+  vehiculo_chasis: string;
+  vehiculo_anio: string;
+  vehiculo_color: string;
+  vehiculo_placa: string;
+  vehiculo_modelo: string;
+
+  // DEL PRESENTANTE
+  presentante_empresa: string;
+  presentante_persona: string;
+  tipo_boleta: string;
+  numero_boleta: string;
+  fecha_boleta: string;
+  dua: string;
+  num_formato_inmatriculacion: string;
+
+  // CLAUSULA DE CANCELACIÓN
+  clausula_monto: string;
+  clausula_forma_pago: string;
+  clausula_pago_bancarizado: string;
+
+  // ACLARACIÓN
+  aclaracion_dice: string;
+  aclaracion_debe_decir: string;
+
+  // SOLO PARA IMPRESION
+  fecha_impresion: string;
+}
+
 // Props para componentes compartidos específicos de trámites
 export interface FormFieldProps {
   label: string;
