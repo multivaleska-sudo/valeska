ee0489d82b429f4da24e5a31a1b2e56e870ba5f2
LavenderEdit <gercermagden@gmail.com>
2026-03-16 19:09:20 -0500

    config: added new configuration for rust to create the local db

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,6 @@
 mod logic;
 use log::LevelFilter;
+use tauri_plugin_sql::{Builder as SqlBuilder, Migration, MigrationKind};
 
 #[tauri::command]
 async fn extract_pdf_data(path: String) -> Result<logic::pdf_parser::ExtractedData, String> {
@@ -12,25 +13,38 @@ async fn extract_xml_data(path: String) -> Result<logic::xml_parser::ExtractedEm
 }
 
 #[tauri::command]
-async fn extract_full_invoice_data(path: String) -> Result<logic::parse_full_ubl_xml::FullInvoiceData, String> {
+async fn extract_full_invoice_data(
+    path: String,
+) -> Result<logic::parse_full_ubl_xml::FullInvoiceData, String> {
     logic::parse_full_ubl_xml::parse_full_ubl_xml(&path)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let migrations = vec![Migration {
+        version: 1,
+        description: "crear_tablas_iniciales",
+        sql: include_str!("../../src/app/db/migrations/0000_flashy_bullseye.sql"),
+        kind: MigrationKind::Up,
+    }];
+
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::default().build())
+        .plugin(
+            SqlBuilder::default()
+                .add_migrations("sqlite:valeska.db", migrations)
+                .build(),
+        )
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_shell::init()) 
+        .plugin(tauri_plugin_shell::init())
         .plugin(
             tauri_plugin_log::Builder::default()
                 .level(LevelFilter::Info)
                 .build(),
         )
         .invoke_handler(tauri::generate_handler![
-            extract_pdf_data, 
+            extract_pdf_data,
             extract_xml_data,
-            extract_full_invoice_data 
+            extract_full_invoice_data
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
